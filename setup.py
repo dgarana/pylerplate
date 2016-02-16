2389205f0ad2511f11cff81c287daef7588b0c70
David Garaña <david.garana@7ideas.com>
2016-02-16 11:29:18 +0100

    Add basic setup.py script
    Add basic setup.py with setuptools bootstrapped on ez_setup.py .

---
+++ setup.py
@@ -0,0 +1,66 @@
+# https://raw.githubusercontent.com/pypa/sampleproject/master/setup.py
+
+# Although pypa's sampleproject does not bootstrap setuptools, we do, because
+# we might want to run this on Python installations which do not have
+# setuptools already installed.
+#
+# See http://pythonhosted.org/setuptools/using.html for further details
+
+try:
+    import setuptools
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+
+import os
+
+import codecs  # To use a consistent encoding
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the relevant file
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+# Put here your dependencies for your application
+# TODO: This should be filled with requirements/prod-requirements.txt
+install_requires = []
+# TODO: This should be filled with requirements/dev-requirements.txt
+develop_requires = []
+
+setuptools.setup(
+    name='your_script',
+    version=':versiontools:your_script:',
+
+    description='YOUR SCRIPT description, fits here',
+    long_description=long_description,
+
+    url='https://dgarana.github.io',
+
+    author='YOUR NAME',
+    author_email='you@yourcompany.com',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='your script keywords',
+
+    packages=setuptools.find_packages(),
+
+    install_requires=install_requires,
+    setup_requires=['versiontools'],
+
+    entry_points={
+        'console_scripts': [
+            'your_script=your_script.main:main',
+        ],
+    },
+)

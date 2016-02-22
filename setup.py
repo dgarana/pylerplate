10a7168800bacaba09f0539cc5d1bfe3df953735
David Garaña <david.garana@7ideas.com>
2016-02-22 09:33:20 +0100

    fix #2: Autofill requirements from file
    Autofill requirements directly from file, instead of hardcoding it
    directly on setup.py.

--- setup.py
+++ setup.py
@@ -15,19 +15,38 @@ except ImportError:
 import os
 
 import codecs  # To use a consistent encoding
+from pip import download
+from pip import req
 
-here = os.path.abspath(os.path.dirname(__file__))
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+def get_requirements(reqfile):
+    path = os.path.join(HERE, reqfile)
+    deps = set()
+    for dep in req.parse_requirements(path, session=download.PipSession()):
+        specs = ','.join(''.join(spec) for spec in dep.req.specs)
+        requirement = '{name}{extras}{specs}'.format(
+            name=dep.name,
+            extras=(
+                '[{extras}]'.format(extras=','.join(dep.extras))
+                if dep.extras else ''
+            ),
+            specs=specs,
+        )
+
+        deps.add(requirement)
+    return deps
 
 # Get the long description from the relevant file
-with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with codecs.open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
 # Put here your dependencies for your application
 # TODO: This should be filled with requirements/prod-requirements.txt
-install_requires = []
+install_requires = get_requirements('requirements/requirements.txt')
 # TODO: This should be filled with requirements/dev-requirements.txt
-develop_requires = []
+develop_requires = get_requirements('requirements/dev-requirements.txt')
 
 setuptools.setup(
     name='your_script',
